5ab3790155981d5d3fd28b15a2884c02c0161f65
Jean Ramalho <jeanramalho.dev@gmail.com>
2024-04-18 12:40:14 -0300

    app[add] - Adicionando eventos ao botão

--- app/src/main/java/com/rocketx/kotlinproject/MainActivity.kt
+++ app/src/main/java/com/rocketx/kotlinproject/MainActivity.kt
@@ -2,10 +2,17 @@ package com.rocketx.kotlinproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun clique(view: View) {
+        println("foi clicado")
+    }
+
+
 }
